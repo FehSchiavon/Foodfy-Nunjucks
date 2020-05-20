5bbf631be013e1c0598d17939d8601082ef77f10
FehSchiavon <felipschiavon@gmail.com>
2020-05-20 08:14:13 -0300

    core: Todos botoes funcinando, agora precisa fazer consumir dados

--- public/scripts.js
+++ public/scripts.js
@@ -1,7 +1,6 @@
 //=== MOSTRAR E ESCONDER EM MAIS BOTOES===//
 
 document.addEventListener('click', toggleDocs, true)
-// let buttonDescription = document.querySelector('.stuff-button')
 
 function toggleDocs (event) {
     if (event.target && event.target.className == 'stuff-button') {
@@ -10,33 +9,36 @@ function toggleDocs (event) {
 
         if (next.style.display == 'block') {
             next.style.display = 'none'
-            // buttonDescription.innerHTML = "MOSTRAR"
-
+            
         } else {
             next.style.display = 'block'
-            // buttonDescription.innerHTML = "ESCONDER"
         }
     }
 }
 
+function ingridient() {
+    let ingridient = document.querySelector('#ingridient-position')
+    if (ingridient.innerHTML === "ESCONDER") {
+        ingridient.innerHTML = "MOSTRAR";
+    } else {
+        ingridient.innerHTML = "ESCONDER";
+    }
+}
 
+function preparation() {
+    let preparation = document.querySelector('#preparation-position')
+    if (preparation.innerHTML === "ESCONDER") {
+        preparation.innerHTML = "MOSTRAR";
+    } else {
+        preparation.innerHTML = "ESCONDER";
+    }
+}
 
-
-// document.addEventListener('click', toggleDocs, true);
-
-// function toggleDocs(event) {
-
-//     if (event.target && event.target.className == 'stuff-button') {
-
-//         var next = event.target.nextElementSibling;
-
-
-//         if (next.style.display == "none") {
-//             next.style.display = "block";
-
-//         } else {
-//             next.style.display = "none";
-//         }
-//     }
-// }
-
+function extra() {
+    let extra = document.querySelector('#extra-position')
+    if (extra.innerHTML === "ESCONDER") {
+        extra.innerHTML = "MOSTRAR";
+    } else {
+        extra.innerHTML = "ESCONDER";
+    }
+}
